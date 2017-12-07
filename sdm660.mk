35f2da27d1f8772578842842fc05767bcc289e95
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    sdm660-common: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- sdm660.mk
+++ sdm660.mk
@@ -71,6 +71,11 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.display.config@1.0
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Init
 PRODUCT_PACKAGES += \
     init.qcom.rc \

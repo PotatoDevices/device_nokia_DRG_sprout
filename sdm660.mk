fc218d2612d8cd5eed0bb22ef73767ff8910c714
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:55:29 +0800

    sdm660-common: device: Build rcs packages
    Change-Id: Idfc57ee69e8d0d7f35528c80f4ce7286b3501f5e

--- sdm660.mk
+++ sdm660.mk
@@ -96,6 +96,13 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # Telephony-ext
 PRODUCT_PACKAGES += \
     telephony-ext

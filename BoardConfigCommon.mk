335fff5e17158caf56b29643eb553167dd479359
LuK1337 <priv.luk@gmail.com>
2018-10-14 14:47:55 +0200

    sdm660-common: Unset PRODUCT_FULL_TREBLE_OVERRIDE
    * It's not needed since it's automatically set for any devices
      shipped with 26+.
    
    Change-Id: I59e17fe3774af63dbea424be6206b77e0e6050ba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,9 +101,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
-# Split selinux policy
-PRODUCT_FULL_TREBLE_OVERRIDE := true
-
 # Vendor
 TARGET_COPY_OUT_VENDOR := vendor
 

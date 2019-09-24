576e6a8218a973733a833ac093449614ccd595f2
theimpulson <aayushgupta219@gmail.com>
2019-09-24 13:12:58 +0000

    sdm660-common: Use unused second system partition during updates
    *Replicate stock behaviour as properties are already installed vendor/build.prop
    
    Change-Id: I014888b8a2eb8092eb3326182e19c80d3343cf6c

--- sdm660.mk
+++ sdm660.mk
@@ -49,7 +49,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
-    otapreopt_script
+    otapreopt_script \
+    cppreopts.sh
 
 # AID/fs configs
 PRODUCT_PACKAGES += \

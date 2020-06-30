44481bb5ce679e87a0a0f413e29568f76368027c
Aayush Gupta <aayushgupta219@gmail.com>
2020-06-30 17:09:16 +0000

    sdm660-common: Update keylayout files from 'Plate2_00WW 10 QKQ1.190828.002 00WW_4_10C release-keys'
    Also move them into vendor
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I586a5a7b0bf0f709da881374832988e0d28ebd17

--- sdm660.mk
+++ sdm660.mk
@@ -230,8 +230,10 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
-    $(COMMON_PATH)/keylayout/goodix_fp.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/goodix_fp.kl
+    $(COMMON_PATH)/keylayout/elan.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/elan.kl \
+    $(COMMON_PATH)/keylayout/ff_key.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ff_key.kl \
+    $(COMMON_PATH)/keylayout/goodix_fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_fp.kl \
+    $(COMMON_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
 # Lights
 PRODUCT_PACKAGES += \

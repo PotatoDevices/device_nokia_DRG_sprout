d24ba33ab3cffad0fc3ab4e52b4f9548d9897545
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-09-11 12:10:34 +0530

    DRG: Prepare tree for building

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, vendor/nokia/DRG_sprout/DRG_sprout-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+		NoCutoutOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
---
+++ lineage_DRG_sprout.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from  device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2280
+
+PRODUCT_BRAND := Nokia
+PRODUCT_DEVICE := DRG_sprout
+PRODUCT_MANUFACTURER := HMD Global
+PRODUCT_NAME := lineage_DRG_sprout
+PRODUCT_MODEL := Nokia 6.1 Plus
+
+PRODUCT_GMS_CLIENTID_BASE := android-hmd
+
+TARGET_VENDOR := nokia
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=Dragon_00WW \
+    PRIVATE_BUILD_DESC="Dragon_00WW 10 QKQ1.190828.002 00WW_4_15C release-keys"
+
+BUILD_FINGERPRINT := Nokia/Dragon_00WW/DRG_sprout:10/QKQ1.190828.002/00WW_4_15C:user/release-keys

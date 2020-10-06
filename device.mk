eb7f343031be49942e6f356e8c77d898a25329f4
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-06 04:40:58 +0000

    sdm660-common: Cleanup graphics stack implementation
    To-do: regenerate up to date proprietary-files.txt
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I9ccefe9806fa4d64f3c6ef132c825023b30dcd36

--- device.mk
+++ device.mk
@@ -183,11 +183,11 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     copybit.sdm660 \
@@ -195,14 +195,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
-    libgenlock \
-    libicuuc.vendor \
-    libgui_vendor \
     liboverlay \
     libqdMetaData.system \
-    libvulkan \
-    libtinyxml \
-    vendor.display.config@1.0
+    libtinyxml
 
 # DRM
 PRODUCT_PACKAGES += \

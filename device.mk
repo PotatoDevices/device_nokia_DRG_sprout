40949d004ca775bb98d2a520fd42c59aa8c26907
SahilSonar <sss.sonar2003@gmail.com>
2020-10-22 16:09:11 +0000

    sdm600-common: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- device.mk
+++ device.mk
@@ -310,7 +310,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 # FIXME: master: compat for libprotobuf

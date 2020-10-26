3c6af53e2e95f2537b388c4dd8afff31b4a3ee6d
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-10-26 18:59:24 +0530

    Revert "DRG: drop my Audio hack"
    This reverts commit 7abcffbc76f96521c40ae7346c18151e1bd44cb4.

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+		NoCutoutOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -128,6 +128,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+		$(DEVICE_PATH)/audio/audio_policy_configuration_2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Bluetooth

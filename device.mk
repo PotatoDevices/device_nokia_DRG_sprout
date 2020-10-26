ec51a5e73ca783442ca5b0e4d6a0e67df3a4bc96
Rahul Krishna <rahulkrishna585@gmail.com>
2020-10-26 21:11:46 +0530

    sdm660-common: Build QTI thermal 2.0 hal
    Change-Id: I4d66a57a1fa162c1018ef5d13dee3346cf157a98
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- device.mk
+++ device.mk
@@ -391,9 +391,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
+    android.hardware.thermal@2.0-service.qti
 
 # Trust
 PRODUCT_PACKAGES += \

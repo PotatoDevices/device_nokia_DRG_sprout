6e7770ed04d54bb51903aa1978e5c9a606349182
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-10 01:17:23 -0600

    Cesium-ify!
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_DRG_sprout.mk \
-  	$(LOCAL_DIR)/bootleg_DRG_sprout.mk
+  	$(LOCAL_DIR)/bootleg_DRG_sprout.mk \
+    $(LOCAL_DIR)/cesium_DRG_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_DRG_sprout-user \
@@ -23,4 +24,7 @@ COMMON_LUNCH_CHOICES := \
     lineage_DRG_sprout-eng \
 	  bootleg_DRG_sprout-user \
     bootleg_DRG_sprout-userdebug \
-    bootleg_DRG_sprout-eng
+    bootleg_DRG_sprout-eng \
+    cesium_DRG_sprout-eng \
+    cesium_DRG_sprout-userdebug \
+    cesium_DRG_sprout-user

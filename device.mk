d0f27f43a8a1d96af1b31c814ab5d355521e0364
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2021-04-02 18:48:09 +0100

    sdm660-common: Use stock volume curves
    * The volume curves defined for the device speakers in
      LineageOS's device-independent android_frameworks_av
      repository are louder than the volume curves on the
      stock firmware, as observed on the Nokia 6.1 (PL2).
      Whether that is healthy for the device's speakers is
      questionable.
    
    * This change imports `audio_policy_volumes.xml` from
      PL2's stock package.
    
    Reference: frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml
    Change-Id: I2774b2660be258fcb5774cd0d22af481b82282f5

--- device.mk
+++ device.mk
@@ -128,13 +128,13 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
+    $(DEVICE_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(DEVICE_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(DEVICE_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
     $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
